9b7500f6420689f5e63a5df2952cd4b12e7c9899
Lorris Saint-Genez <lorrissaintgenez@gmail.com>
2025-08-07 15:35:27 -0700

    feat(Index): clear index operation (#9)

---
+++ nodes/Algolia/index/clearIndex.ts
@@ -0,0 +1,18 @@
+import { Operation } from '@/helpers';
+
+import { indexName } from '../shared/indexName.field';
+
+export const clearIndex = new Operation({
+	name: 'Clear Index',
+	action: 'Clear index',
+	value: 'clearIndex',
+	description: 'Clear all records from an index without affecting its settings, synonyms, or rules',
+	routing: {
+		request: {
+			method: 'POST',
+			url: '=/1/indexes/{{ $parameter.indexName }}/clear',
+			json: true,
+		},
+	},
+})
+	.addField(indexName);
